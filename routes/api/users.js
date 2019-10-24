fabe5a376391f4cfd86b5e6d7e566e79d445b8fb
Thanh Nguyen <tnguyen@GBLONLAP0112.local>
2019-10-24 11:58:20 +0100

    check if user exist

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator/check");
+
+const User = require("../../models/User");
 // @route    POST api/users
 // @desc     Register users
 // @access   Public
@@ -13,13 +15,29 @@ router.post(
     check("email", "Please include a valid email").isEmail(),
     check("password", "Please enter a password with 6 or more characters").isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    const { name, email, password } = req.body;
 
-    res.send("User route");
+    try {
+      //See if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+      // Get users gravatar
+
+      // Encrypt password
+
+      // Return jsonwebtoken
+      res.send("User route");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
