945fcc59e5883e319bec19bbc2b7bda10f087fbf
Thanh Nguyen <tnguyen@GBLONLAP0112.local>
2019-10-24 13:56:20 +0100

    get user gravatar and encrypt password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../../models/User");
@@ -42,11 +43,12 @@ router.post(
         avatar,
         password
       });
-
       // Encrypt password
-
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
       // Return jsonwebtoken
-      res.send("User route");
+      res.send("User registered");
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
