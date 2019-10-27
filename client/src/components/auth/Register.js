9919194ca57e19ebdb70c2035db01e5910fe19ea
Thanh Nguyen <tnguyen@GBLONLAP0112.local>
2019-10-27 21:58:52 +0000

    call register action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,11 @@
 import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
 import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -21,7 +22,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert("Passwords do not match", "danger");
     } else {
-      console.log("success");
+      register({ name, email, password });
     }
   };
   return (
@@ -68,9 +69,10 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 };
 export default connect(
   null,
-  { setAlert }
+  { setAlert, register }
 )(Register);
